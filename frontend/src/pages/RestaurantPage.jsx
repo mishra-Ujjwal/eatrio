1f7264975e40c90bc44995017d00a75d8fd44b73
mishra-ujjwal <ujjwalmishra9717@gmail.com>
2025-10-29 22:14:14 +0530

    Fix: moved utils inside src for Render build

--- frontend/src/pages/RestaurantPage.jsx
+++ frontend/src/pages/RestaurantPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import axios from "axios";
-import { capitalizeFirstLetter } from "../../utils/capitalizeFirstLetter.jsx";
+import { capitalizeFirstLetter } from "../utils/capitalizeFirstLetter.jsx";
 
 const RestaurantPage = () => {
   const { id } = useParams();
